7f979d48e1e35674fc61cad62b7638a4baf6d072
Kabin Nam <kabinnam@gmail.com>
2018-03-10 22:49:04 -0800

    converting to iPython notebooks, for easier viewing on github

--- Ch01_Arrays_and_Strings/Python/2/2.py
+++
@@ -1,7 +0,0 @@
-# Check Permutation: Given two strings,
-# write a method to decide if one is a
-# permutation of the other.
-#
-# Hints: #1, #84, #122, #131
-
-
--- Ch01_Arrays_and_Strings/Python/3/3_test.py
+++
@@ -1,4 +0,0 @@
-# Test Program to cover and check as many cases as possible, that would be
-# reasonable to check within an interview.
-
-

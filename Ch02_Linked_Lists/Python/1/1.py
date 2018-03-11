db6b83148954638b70e99654d995aea230a21ed3
Kabin Nam <kabinnam@gmail.com>
2018-03-10 23:34:49 -0800

    Fleshing out repository structure

--- Ch02_Linked_Lists/Python/1/1.py
+++
@@ -1,7 +0,0 @@
-# Remove Dups: write code to remove duplicates from an unsorted linked list.
-# FOLLOW UP
-# How would you solve this problem if a temporary buffer is not allowed?
-#
-# Hints: #9, #40
-
-
--- Ch02_Linked_Lists/Python/2/2.py
+++
@@ -1,6 +0,0 @@
-# Return Kth to Last: Implement an algorith to find the kth to last element
-# of a singly linked list.
-#
-# Hints: #8, #25, #41, #67, #126
-
-
